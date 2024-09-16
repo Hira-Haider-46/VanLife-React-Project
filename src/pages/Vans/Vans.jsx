bece457d26bd6519d6f67f5558c58c5e9f90a84d
Hira <hirahaiderali1975@gmail.com>
2024-09-16 17:43:25 +0500

    error handling in vans page

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,35 +1,53 @@
 import React from "react"
-import { Link, useSearchParams } from "react-router-dom";
-import './Vans.css';
+import { Link, useSearchParams } from "react-router-dom"
+import { getVans } from "../../api.js"
+import './Vans.css'
 
 export default function Vans() {
     const [searchParams, setSearchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
 
     const typeFilter = searchParams.get("type")
 
     React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getVans()
+                setVans(data)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+        loadVans()
     }, [])
 
     const displayedVans = typeFilter
         ? vans.filter(van => van.type === typeFilter)
         : vans
 
-        const vanElements = displayedVans.map(van => (
-            <div key={van.id} className="van-tile">
-                <Link to={van.id} state={{ search: `?${searchParams.toString()}`, type: typeFilter }}>
-                    <img src={van.imageUrl} />
-                    <div className="van-info">
-                        <h3>{van.name}</h3>
-                        <p>${van.price}<span>/day</span></p>
-                    </div>
-                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
-                </Link>
-            </div>
-        ))
+    const vanElements = displayedVans.map(van => (
+        <div key={van.id} className="van-tile">
+            <Link
+                to={van.id}
+                state={{
+                    search: `?${searchParams.toString()}`,
+                    type: typeFilter
+                }}
+            >
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
+        </div>
+    ))
 
     function handleFilterChange(key, value) {
         setSearchParams(prevParams => {
@@ -42,6 +60,14 @@ export default function Vans() {
         })
     }
 
+    if (loading) {
+        return <h1 aria-live="polite">Loading...</h1>
+    }
+
+    if (error) {
+        return <h1 aria-live="assertive">There was an error: {error.message}</h1>
+    }
+
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
@@ -49,19 +75,22 @@ export default function Vans() {
                 <button
                     onClick={() => handleFilterChange("type", "simple")}
                     className={
-                        `van-type simple ${typeFilter === "simple" ? "selected" : ""}`
+                        `van-type simple 
+                        ${typeFilter === "simple" ? "selected" : ""}`
                     }
                 >Simple</button>
                 <button
                     onClick={() => handleFilterChange("type", "luxury")}
                     className={
-                        `van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`
+                        `van-type luxury 
+                        ${typeFilter === "luxury" ? "selected" : ""}`
                     }
                 >Luxury</button>
                 <button
                     onClick={() => handleFilterChange("type", "rugged")}
                     className={
-                        `van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`
+                        `van-type rugged 
+                        ${typeFilter === "rugged" ? "selected" : ""}`
                     }
                 >Rugged</button>
 
