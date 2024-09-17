c022bc220f465e5d96eef127232d880a18127d59
Hira <hirahaiderali1975@gmail.com>
2024-09-17 18:10:38 +0500

    login issue fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,52 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import React from "react"
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import logo from '../../assets/images/avatar-icon.png';
 import './Header.css';
 
-function Header() {
-    const styles = {
-        "font-weight": "bold",
-        "text-decoration": "underline",
-        "color": "#161616"
-    };
+export default function Header() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
 
+    const navigate = useNavigate();
+
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+        navigate('/login')
+    }
+    
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <NavLink to="/host" className={({ isActive }) => isActive ? "active-link" : null}>Host</NavLink>
-                <NavLink to="/about" className={({ isActive }) => isActive ? "active-link" : null}>About</NavLink>
-                <NavLink to="/vans" className={({ isActive }) => isActive ? "active-link" : null}>Vans</NavLink>
+                <NavLink 
+                    to="/host"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    Host
+                </NavLink>
+                <NavLink 
+                    to="/about"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    About
+                </NavLink>
+                <NavLink 
+                    to="/vans"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
                 <Link to="login" className="login-link">
-                    <img
+                    <img 
                         src={logo}
                         className="login-icon"
                     />
                 </Link>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
 }
-
-export default Header;
