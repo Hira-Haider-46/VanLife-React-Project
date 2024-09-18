1f114b3261d6636c1b858311e8e093cd99d9c81e
Hira <hirahaiderali1975@gmail.com>
2024-09-18 18:54:07 +0500

    integrated with firebase

--- src/api.js
+++ src/api.js
@@ -1,33 +1,46 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(() => resolve(), ms))
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, doc, getDoc, query, where } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "react-vanlife-project.firebaseapp.com",
+  projectId: "react-vanlife-project",
+  storageBucket: "react-vanlife-project.appspot.com",
+  messagingSenderId: "35842905783",
+  appId: "1:35842905783:web:d8f5b4ae490ce7acdedf32"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans;
 }
 
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
     }
-    const data = await res.json()
-    return data.vans
 }
 
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
 }
 
 export async function loginUser(creds) {
--- src/pages/VanDetails/VanDetails.jsx
+++ src/pages/VanDetails/VanDetails.jsx
@@ -1,18 +1,37 @@
 import React from "react"
 import { Link, useParams, useLocation } from "react-router-dom"
+import { getVan } from "../../api.js";
 import './VanDetails.css';
 
 export default function VanDetails() {
-    const params = useParams()
-    const location = useLocation()
-    
     const [van, setVan] = React.useState(null)
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
+    const { id } = useParams()
+    const location = useLocation()
 
     React.useEffect(() => {
-        fetch(`/api/vans/${params.id}`)
-            .then(res => res.json())
-            .then(data => setVan(data.vans))
-    }, [params.id])
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getVan(id)
+                setVan(data)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+        loadVans()
+    }, [id])
+    
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
+    }
 
     const search = location.state?.search || "";
     const type = location.state?.type || "all";
@@ -23,9 +42,9 @@ export default function VanDetails() {
                 to={`..${search}`}
                 relative="path"
                 className="back-button"
-            >&larr; <span>{`Back to ${type} vans`}</span></Link>
+            >&larr; <span>Back to {type} vans</span></Link>
             
-            {van ? (
+            {van && (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
                     <i className={`van-type ${van.type} selected`}>
@@ -36,7 +55,7 @@ export default function VanDetails() {
                     <p>{van.description}</p>
                     <button className="link-button">Rent this van</button>
                 </div>
-            ) : <h2>Loading...</h2>}
+            )}
         </div>
     )
 }
